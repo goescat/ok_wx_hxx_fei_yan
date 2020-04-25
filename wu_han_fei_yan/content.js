7e5eaf0e3610c06f022920a7ea3ab80556eb2fa5
goescat <goescat1024@gmail.com>
2020-04-25 23:53:56 +0800

    Add files via upload

---
+++ wu_han_fei_yan/content.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function(){
+    document.body.innerHTML = document.body.innerHTML.replace(/新冠/g, '武漢');
+    document.body.innerHTML = document.body.innerHTML.replace(/新型冠狀/g, '武漢');
+    document.body.innerHTML = document.body.innerHTML.replace(/COVID-19/g, '武漢肺炎');
+    document.body.innerHTML = document.body.innerHTML.replace(/covid-19/g, '武漢肺炎');
+});
